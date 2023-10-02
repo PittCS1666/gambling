eeb8752296461cf911f5236c379c4eb5722662b9
sam-lb <samlbrunacini@gmail.com>
2023-10-02 03:01:39 -0400

    implemented dealing and stuff, moved code for shuffling, dealing, and card generation to systems.rs

--- school_sanctioned_gambling/src/main.rs
+++ school_sanctioned_gambling/src/main.rs
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-use bevy::{prelude::*, window::PresentMode, transform};
-use rand::thread_rng;
-use rand::seq::SliceRandom;
-=======
 use bevy::{prelude::*, window::PresentMode};
 
 mod credits;
@@ -12,81 +7,11 @@ mod game;
 use game::*;
 use menu::*;
 use credits::*;
->>>>>>> upstream/main
 
 const TITLE: &str = "School Sanctioned Gambling";
 const WIN_WIDTH: f32 = 1280.;
 const WIN_HEIGHT: f32 = 720.;
 
-<<<<<<< HEAD
-#[derive(Component, Deref, DerefMut)]
-struct SlideTimer(Timer);
-
-#[derive(Copy, Clone, PartialEq)]
-enum Suit {
-    Hearts,
-    Diamonds,
-    Spades,
-    Clubs
-}
-
-struct Card {
-    card_id: u8, // unique card id: hearts 0-12, diamonds 13-25, spades 26-38, clubs 39-51
-    suit: Suit,
-    value: u8, // ace: 1, 2: 2, ..., 10: 10, jack: 11, queen: 12, king: 13
-}
-
-impl Card {
-    fn new(card_id: u8, suit: Suit, value: u8) -> Card {
-        Card {
-            card_id,
-            suit,
-            value,
-        }
-    }
-
-    fn to_string(&self) -> String {
-        String::from(format!("Card({id}, {suit}, {value})", 
-        id=self.card_id, value=self.value, suit={
-            if self.suit == Suit::Hearts {
-                "Hearts"
-            } else if self.suit == Suit::Diamonds {
-                "Diamonds"
-            } else if self.suit == Suit::Spades {
-                "Spades"
-            } else {
-                "Clubs"
-            }
-        }))
-    }
-}
-
-
-fn init_cards() -> Vec<Card> {
-    let mut cards: Vec<Card> = Vec::with_capacity(52);
-    let mut total: u8 = 0;
-    let suits: Vec<Suit> = vec![Suit::Hearts, Suit::Diamonds, Suit::Spades, Suit::Clubs];
-    for suit in suits {
-        for value in 0..13 {
-            cards.push(Card::new(total, suit, value));
-            total += 1;
-        }
-    }
-    cards
-}
-
-fn display_cards(cards: Vec<Card>) {
-    for card in cards {
-        println!("{}", card.to_string());
-    }
-}
-
-fn shuffle_cards(mut cards: Vec<Card>) {
-    cards.shuffle(&mut thread_rng());        
-}
-
-=======
->>>>>>> upstream/main
 
 #[derive(States, Debug, Clone, Copy, Eq, PartialEq, Hash, Default)]
 pub enum AppState {
@@ -98,9 +23,6 @@ pub enum AppState {
 }
 
 fn main() {
-    let cards: Vec<Card> = init_cards();
-    display_cards(cards);
-
     App::new()
         .add_state::<AppState>()
         .add_plugins((
