392da30f6f916a8e44a2fd0c34b5dadc284e7ee5
Garrett A Barber <79263753+GAB8202@users.noreply.github.com>
2023-10-02 11:58:56 -0400

    Can switch between game and menu

--- school_sanctioned_gambling/src/game/components.rs
+++ school_sanctioned_gambling/src/game/components.rs
@@ -2,3 +2,12 @@ use bevy::prelude::*;
 
 #[derive(Component)]
 pub struct Background;
+
+#[derive(Component)]
+pub struct CheckButton;
+
+#[derive(Component)]
+pub struct NBundle;
+
+#[derive(Component)]
+pub struct Camera;
--- school_sanctioned_gambling/src/game/systems.rs
+++ school_sanctioned_gambling/src/game/systems.rs
@@ -1,11 +1,110 @@
 use bevy::prelude::*;
 use super::components::*;
-
+use crate::AppState;
 
 pub fn load_game(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2dBundle::default()).insert(Camera);
     commands.spawn(SpriteBundle {
         texture: asset_server.load("game_screen.png"),
         transform: Transform::from_xyz(0., 0., 1.),
         ..default()
     }).insert(Background);
+    spawn_buttons(&mut commands, &asset_server);
+}
+
+fn spawn_buttons(commands: &mut Commands, asset_server: &Res<AssetServer>) {
+    commands
+        .spawn(NodeBundle {
+            style: Style {
+                width: Val::Percent(100.0),
+                align_items: AlignItems::Center,
+                justify_content: JustifyContent::Center,
+                ..default()
+            },
+            ..default()
+        }).insert(NBundle)
+        .with_children(|parent| {
+            //spawn check button
+            parent.spawn(ButtonBundle {
+                style: Style {
+                    position_type: PositionType::Absolute,
+                    top: Val::Px(600.),
+                    width: Val::Px(230.0),
+                    height: Val::Px(90.0),
+                    border: UiRect::all(Val::Px(3.0)),
+                    // horizontally center child text
+                    justify_content: JustifyContent::Center,
+                    // vertically center child text
+                    align_items: AlignItems::Center,
+                    ..default()
+                },
+                border_color: BorderColor(Color::BLACK),
+                background_color: Color::rgb(0.071, 0.141, 0.753).into(),
+                ..default()
+            }).insert(CheckButton)
+            .with_children(|parent| {
+                parent.spawn(TextBundle::from_section(
+                    "Check",
+                    TextStyle {
+                        font: asset_server.load("fonts/Lato-Black.ttf"),
+                        font_size: 40.0,
+                        color: Color::rgb(0.9, 0.9, 0.9),
+                    },
+                ));
+            });
+        });
+}
+
+pub fn tear_down_game_screen(
+    mut commands: Commands, 
+    mut background_query: Query<Entity, With<Background>>, 
+    mut button_query: Query<Entity, With<Button>>,
+    mut node_query: Query<Entity, With<NBundle>>,
+    mut camera_query: Query<Entity, With<Camera>>,) 
+{
+    for button in &mut button_query {
+        commands.entity(button).despawn_recursive();
+    }
+
+    let node = node_query.single_mut();
+
+    commands.entity(node).despawn_recursive();
+
+    let background = background_query.single_mut();
+    
+    commands.entity(background).despawn_recursive();
+    //commands.entity(exit_button).despawn_recursive();
+
+    let camera_entity = camera_query.single_mut();
+    commands.entity(camera_entity).despawn_recursive();
+}
+
+pub fn check_button_interaction(
+    mut interaction_query: Query<
+    (
+        &Interaction,
+        &mut BackgroundColor,
+        &mut BorderColor,
+    ),
+    (Changed<Interaction>, With<CheckButton>),
+    >,
+    mut app_state_next_state: ResMut<NextState<AppState>>,
+) {
+    for (interaction, mut color, mut border_color) in &mut interaction_query {
+        match *interaction {
+            Interaction::Pressed => {
+                *color = Color::rgb(0.075, 0.118, 0.502).into();
+                border_color.0 = Color::RED;
+                app_state_next_state.set(AppState::MainMenu);
+            }
+            Interaction::Hovered => {
+                *color = Color::rgb(0.133, 0.188, 0.659).into();
+                border_color.0 = Color::WHITE;
+            }
+            Interaction::None => {
+                *color = Color::rgb(0.071, 0.141, 0.753).into();
+                border_color.0 = Color::BLACK;
+            }
+        }
+    }
 }
