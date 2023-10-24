0673d70cd9919e10f6f5d9ed5a312d684a9d9098
mjg146 <MJG146@pitt.edu>
2023-10-23 22:24:13 -0400

    fixed errors from merge

--- school_sanctioned_gambling/src/game/cards.rs
+++ school_sanctioned_gambling/src/game/cards.rs
@@ -95,7 +95,7 @@ pub fn shuffle_cards(cards: &mut Vec<Card>) {
     cards.shuffle(&mut thread_rng());
 }
 
-pub fn deal_hands(player_count: usize, cards: &mut Vec<Card>, poker_turn: PokerTurn, community_cards: CommunityCards) -> Vec<Player> {
+pub fn deal_hands(player_count: usize, cards: &mut Vec<Card>) -> Vec<Player> {
     let mut result: Vec<Player> = Vec::with_capacity(player_count as usize);
     for player_id in 0..player_count {
         let hand: Vec<Card> = cards.drain(0..2).collect();
@@ -108,8 +108,8 @@ pub fn deal_hands(player_count: usize, cards: &mut Vec<Card>, poker_turn: PokerT
             has_moved: false,
             is_all_in: false,
             has_raised: false,
-            hand_strength: generate_hand_strength(&hand, poker_turn, community_cards),
-            move_dist: fill_move_set(poker_turn.phase),
+            hand_strength: generate_pre_flop_hand_strength(&hand),
+            move_dist: fill_move_set(),
             big_blind: false,
             small_blind: false,
         });
@@ -168,7 +168,7 @@ pub fn card_function(
     }
 }
 
-pub fn spawn_player_cards(commands: &mut Commands, asset_server: &Res<AssetServer>, players: &Vec<Player>, query: &mut Query<(Entity, &mut Player)>, poker_turn: PokerTurn, community_cards: CommunityCards) {
+pub fn spawn_player_cards(commands: &mut Commands, asset_server: &Res<AssetServer>, players: &Vec<Player>, query: &mut Query<(Entity, &mut Player)>) {
     // If players don't exist create the entity, if they do just update their cards they hold
     for player in players {
         let mut player_exists = false;
@@ -191,8 +191,8 @@ pub fn spawn_player_cards(commands: &mut Commands, asset_server: &Res<AssetServe
                 has_moved: player.has_moved,
                 is_all_in: player.is_all_in,
                 has_raised: player.has_raised,
-                hand_strength: generate_hand_strength(&player.cards, poker_turn, community_cards ),
-                move_dist: fill_move_set(poker_turn.phase),
+                hand_strength: generate_pre_flop_hand_strength(&player.cards),
+                move_dist: fill_move_set(),
                 big_blind: false,
                 small_blind: false,
             });
--- school_sanctioned_gambling/src/game/mod.rs
+++ school_sanctioned_gambling/src/game/mod.rs
@@ -21,7 +21,7 @@ impl Plugin for GamePlugin {
         app
         .insert_resource(init_cards_resource())
         //This line can be used to set whose turn it is
-        .insert_resource(PokerTurn { current_player: 1, phase: PokerPhase::PreFlop, round_started: false, pot: 0, current_top_bet:0, bet_made: false, left_of_dealer: false, small_blind: 0, big_blind: 1, small_blind_val: 25, big_blind_val: 50})
+        .insert_resource(PokerTurn { current_player: 1, phase: PokerPhase::PreFlop, round_started: false, pot: 0, current_top_bet:0, bet_made: false, pot_raised: false, small_blind: 1, big_blind: 0, small_blind_val: 25, big_blind_val: 50})
         //Update this line to increase number of players for now
         .insert_resource(NumPlayers { player_count: 2 })
         .insert_resource(LastPlayerAction{ action: Some(PlayerAction::None) })
