dd7ea10906a34d43f17f0ce1880896bb467436a1
alexhaurin <92339236+alexhaurin@users.noreply.github.com>
2023-10-24 21:12:38 -0400

    Added exit server functionality

--- school_sanctioned_gambling/src/networking/components.rs
+++ school_sanctioned_gambling/src/networking/components.rs
@@ -6,6 +6,9 @@ pub struct NBundle;
 #[derive(Component)]
 pub struct JoinServerButton;
 
+#[derive(Component)]
+pub struct ExitServerButton;
+
 #[derive(Component, Default)]
 pub struct TextBox {
     pub text_style: TextStyle,
