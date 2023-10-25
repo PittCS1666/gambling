b64d154e73ab0a12d6ed269387f998b3dfe6e3c7
alexhaurin <92339236+alexhaurin@users.noreply.github.com>
2023-10-24 21:13:27 -0400

    Added exit server functionality

--- school_sanctioned_gambling/src/networking/mod.rs
+++ school_sanctioned_gambling/src/networking/mod.rs
@@ -16,12 +16,10 @@ impl Plugin for NetworkingPlugin {
         .add_systems(Update, ip_textbox_button_interaction.run_if(in_state(AppState::OnlinePlay)))
         .add_systems(OnExit(AppState::OnlinePlay), remove_gui)
 
-        //.add_systems(OnEnter(AppState::OnlineServer), remove_gui)
-        //.add_systems(OnEnter(AppState::OnlineServer), server_on_enter)
-        //.add_systems(Update, server_on_update.run_if(in_state(AppState::OnlineServer)))
-
         //.add_systems(OnEnter(AppState::OnlineClient), remove_gui)
         .add_systems(OnEnter(AppState::OnlineClient), client_on_enter)
-        .add_systems(Update, client_on_update.run_if(in_state(AppState::OnlineClient)));
+        .add_systems(OnExit(AppState::OnlineClient), remove_gui)
+        .add_systems(Update, client_on_update.run_if(in_state(AppState::OnlineClient)))
+        .add_systems(Update, exit_server_button_interaction.run_if(in_state(AppState::OnlineClient)));
     }
 }
